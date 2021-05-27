92cbb222f8630d6850621f29ad72f68afce6cdb3
Javier Barreras <71753136+Superatons23@users.noreply.github.com>
2021-05-27 09:27:21 -0700

    se agrego pantalla para poder crear asignacion

---
+++ app/src/main/java/apps/moviles/ensenianza/PantallaCrearAsignacion.kt
@@ -0,0 +1,11 @@
+package apps.moviles.ensenianza
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PantallaCrearAsignacion : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pantalla_crear_asignacion)
+    }
+}
